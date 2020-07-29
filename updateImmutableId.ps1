14c85de9affef60dcb5aff669708412e0c824b79
Jarret Redding <3312237+c0nc3ptSF@users.noreply.github.com>
2020-07-28 22:29:24 -0400

    Initial Commit

---
+++ updateImmutableId.ps1
@@ -0,0 +1,30 @@
+param(
+    [string]$upn = $(Read-Host 'Enter UPN'),
+    [bool]$Credentials = $true
+)
+
+try{
+
+    if($Credentials -eq $true){
+
+        $Credential = Get-Credential
+        Connect-MsolService -Credential $Credential
+
+    }
+
+    $GUID = (Get-ADUser -Filter {UserPrincipalName -eq $upn}).ObjectGUID 
+    $ImmutableID = [System.Convert]::ToBase64String($GUID.tobytearray())
+
+    Write-Host "Generated ImmutableID:" $ImmutableID
+
+    Set-MSOLuser -UserPrincipalName $upn -ImmutableID $immutableID
+
+    Write-Host "ImmutableID written to Office 365 User. Please confirm it matches generated ImmutableID"
+
+    Get-MsolUser -UserPrincipalName $upn | ft UserPrincipalName,immutableid,lastdirsync*
+
+} catch {
+
+    Write-Host $_.Exception.Message -ForegroundColor Red -BackgroundColor Black
+
+}
